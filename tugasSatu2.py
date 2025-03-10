8e4df87cd1c1ec6cb6510708cf9a455d19aeca18
tinyDevill <122863594+tinyDevill@users.noreply.github.com>
2025-03-10 22:19:22 +0700

    Delete tugasSatu2.py

--- tugasSatu2.py
+++
@@ -1,8 +0,0 @@
-student_num = int(input("Masukkan jumlah siswa: "))
-student_marks = {}
-
-for i in range(student_num):
-    name = input(f"Masukkan nama siswa ke-{i + 1}: ")
-    student_marks[name] = int(input(f"Masukkan nilai untuk {name}: "))
-
-print(f"dictionary = {student_marks}")
