3b03f91e7819c7a4bf18039a16975f573d17120b
tinyDevill <122863594+tinyDevill@users.noreply.github.com>
2025-03-10 22:25:14 +0700

    Delete tugasSatu3.py

--- tugasSatu3.py
+++
@@ -1,6 +0,0 @@
-with open('Me.txt', 'x') as f:
-    f.write(input("Masukkan nama: "))
-    f.write("\n")
-    f.write(input("Masukkan Nim : "))
-    f.write("\n")
-    f.write(input("Masukkan Resolusi di Tahun ini: "))
